b90c457d27b43b3e6f11e3db76a2290f948016cf
ivocoimbraw <wolfcoimbraivo@gmail.com>
2023-10-23 19:24:25 -0400

    p_disponible v1

--- src/main/java/com/proyectosi1/apirest/entity/ProductoEntity.java
+++ src/main/java/com/proyectosi1/apirest/entity/ProductoEntity.java
@@ -1,12 +1,15 @@
 package com.proyectosi1.apirest.entity;
 
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -44,5 +47,8 @@ public class ProductoEntity {
     @ManyToOne
     @JoinColumn(name = "id_marca")
     private MarcaEntity marca;
+
+    @OneToMany(mappedBy = "producto")
+    private List<ProductoDisponibleEntity> productoDisponible;
 
 }
